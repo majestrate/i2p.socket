054701cc08df2bd342ffd09d31978ca2ba32b9b5
Jeff Becker <ampernand@gmail.com>
2016-07-27 08:03:33 -0400

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if os.path.exists(install_requires_fname):
 if sys.version_info[0] < 3:
     install_requires.append("future>=0.14.0")
 
-version = '0.2'
+version = '0.2.1'
 
 setup(
     name='i2p.socket',
